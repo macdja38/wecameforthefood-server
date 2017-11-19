01e39a65e9077e99e938d80665666fcc82092dd8
macdja38 <jakeincanada@icloud.com>
2017-11-19 00:59:03 -0500

    properly parse input

--- api/item/index.js
+++ api/item/index.js
@@ -6,9 +6,36 @@ routes.post('/', (req, res) => {
   console.log("Got a request");
   console.log(req.body);
 
-  const item = Object.assign({}, req.body);
+  const originalItem = req.body.expire;
 
-  item.expire = r.epochTime(chrono.parseDate(`in ${req.body.expire}`, Date.now()).getTime() / 1000);
+  const item = {};
+
+  try {
+    item.expire = r.epochTime(chrono.parseDate(`in ${originalItem.expire}`, Date.now()).getTime() / 1000)
+  } catch (error) {
+    res.status(400).send("Invalid expire format, try \"1 day\"");
+    return;
+  }
+
+  item.size = parseInt(originalItem.size, 10);
+  if (isNaN(item.size) || item.size === 0) {
+    res.status(400).send("Invalid size, please send a valid non-zero number, eg: 10");
+    return;
+  }
+
+  item.price = parseInt(originalItem.price, 10);
+  if (isNaN(item.price) || item.price === 0) {
+    res.status(400).send("Invalid price, please send a valid non-zero number, eg: 5");
+    return;
+  }
+
+  item.name = originalItem.name;
+  if (typeof item.name !== "string" || item.name === "") {
+    res.status(400).send("Invalid name, please send a string with content, eg bagel");
+    return;
+  }
+
+  // r.db("test").table("list").sum("size").add(5).gt(80000).branch(false, r.db("test").table("list").insert(r.db("test").table("list").get("73b21891-cd97-4e83-b1c2-8f9b10904020").without("id")));
 
   r.db("test").table("list").insert(item).then(result => {
     console.log(result);
