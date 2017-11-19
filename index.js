3550e84483d252a25c10c2526d5d2a23833f42c8
macdja38 <jakeincanada@icloud.com>
2017-11-18 23:24:13 -0500

    during testing allow all origins

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const app = express();
 // parse application/json
 app.use(bodyParser.json());
 
+// during testing allow all origins
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use("/api", api);
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
